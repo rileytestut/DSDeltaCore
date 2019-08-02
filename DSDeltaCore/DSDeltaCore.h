112076b3eb5a264eba2f50a52255ca1293b53b8c
Riley Testut <riley@rileytestut.com>
2019-08-02 12:18:29 -0700

    Initial Commit

---
+++ DSDeltaCore/DSDeltaCore.h
@@ -0,0 +1,19 @@
+//
+//  DSDeltaCore.h
+//  DSDeltaCore
+//
+//  Created by Riley Testut on 8/2/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DSDeltaCore.
+FOUNDATION_EXPORT double DSDeltaCoreVersionNumber;
+
+//! Project version string for DSDeltaCore.
+FOUNDATION_EXPORT const unsigned char DSDeltaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSDeltaCore/PublicHeader.h>
+
+
